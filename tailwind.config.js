c4b1e6e1fdbf9a3401b60dfc341119fe7b08b077
Adhitya Aditama Nugraha <aaditamanugraha@Adhityas-MacBook-Air.local>
2024-06-05 23:58:07 +0700

    feat: add custom colors in tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       minWidth: {
         mobile: "23.438rem",
       },
+      colors: {
+        "primary-blue": "hsl(223, 87%, 63%)",
+        "secondary-pale-blue": "hsl(223, 100%, 88%)",
+        "secondary-light-red": "hsl(354, 100%, 66%)",
+        "neutral-gray": "hsl(0, 0%, 59%)",
+        "neutral-very-dark-blue": "hsl(209, 33%, 12%)",
+      },
     },
   },
   plugins: [],
