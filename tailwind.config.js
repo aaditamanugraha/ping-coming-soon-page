6ca704909e7767917b0fc52b229f3242e77b56ea
Adhitya Aditama Nugraha <aaditamanugraha@Adhityas-MacBook-Air.local>
2024-06-05 23:41:37 +0700

    fix: structure file and folder because can't apply style of tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      minWidth: {
+        mobile: "23.438rem",
+      },
+    },
+  },
+  plugins: [],
+};
